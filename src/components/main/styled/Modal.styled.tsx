bf4243bb6c774cbcd03a1a90bdcfc11f848c0fff
Kkangjn <jjun1401@naver.com>
2023-08-18 21:30:23 +0900

    Feat : reservation

--- src/components/main/styled/Modal.styled.tsx
+++ src/components/main/styled/Modal.styled.tsx
@@ -43,4 +43,14 @@ cursor: pointer;
 }
 `
 
-export { Modal, ModalContent, ModalInput, ModalSubmit };
+const ModalSelect = styled.select`
+width: 100%;
+padding: 12px 20px;
+margin: 8px 0;
+display: inline-block;
+border: 1px solid #ccc;
+border-radius: 4px;
+box-sizing: border-box;
+`
+
+export { Modal, ModalContent, ModalInput, ModalSubmit, ModalSelect };
