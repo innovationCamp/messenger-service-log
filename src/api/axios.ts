a639d4a812cd9fda5248a9f969c9875e824b1e28
hanjungeun0909 <132903726+hanjungeun0909@users.noreply.github.com>
2023-09-04 20:12:59 +0900

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const devInstance = axios.create({
-    // baseURL: "http://3.39.250.124:8080/api",
+    //// baseURL: "http://3.39.250.124:8080/api",
     
     baseURL: `${process.env.SERVER}/api`,
     timeout: 3000,
